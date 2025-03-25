7188af04eba0e329b3adc1c6d1141f58a410d339
anhchi7103 <anhchi7103@gmail.com>
2025-03-26 00:24:50 +0700

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Category from "./pages/Category"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Product from "./pages/Product"
+
+//import images
+import bannermen from "./assets/bannermens.png"
+import bannerwomen from "./assets/bannerwomens.png"
+import bannerkid from "./assets/bannerkids.png"
+
+export default function App() {
+  return (
+    <main className="bg-primary text-tertiary">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/mens" element={<Category category="men" banner={bannermen}/>}/>
+            <Route path="/womens" element={<Category category="women" banner={bannerwomen}/>}/>
+            <Route path="/kids" element={<Category category="kid" banner={bannerkid}/>}/>
+            <Route path="/product/:productId" element={<Product />}/>
+              {/*<Route path=":productId" element={<Product/>}/> */}
+            <Route path="/cart-page" element={<Cart />}/>
+            <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </main>
+  )
+}
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom'
+import { FaStar } from 'react-icons/fa'
+import { MdOutlineLocalOffer } from 'react-icons/md'
+
+const Hero = () => {
+  return (
+    <section className='relative bg-center bg-no-repeat h-screen w-full'>
+        <div className="max_padd_container relative top-10 xs:top-52">
+            <h1 className='h3 max-w-[37rem]'> The perfect way to convert your traffic into sales  </h1>
+            <p className='text-gray-50 regular-16 mt-6 max-w-[33rem]'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam veritatis sapiente ullam animi nisi. Molestiae, nesciunt. Impedit rerum, quibusdam odit dolorem iste, aut illum aliquid facere nobis nostrum dolore exercitationem!
+            </p>
+            <div className='flexStart !items-center gap-x-4 my-10'>
+                <div className='bold-16 sm:bold-20'> 176k <span className='regular-16 sm:regular-20'>Excellent Reviews </span></div>
+            </div>
+            <div className='max-xs:flex-col flex gap-2'>
+                <NavLink to='' className={"btn_dark_rounded flexCenter"}> Buy Now </NavLink>
+                <NavLink to='' className={"btn_dark_rounded flexCenter gap-x-1"}> <MdOutlineLocalOffer className='text-2x1'/> Offers </NavLink>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
